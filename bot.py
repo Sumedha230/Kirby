e20800e8f41059b6273752759d6284579e3642e8
Sumedha230 <100405238+Sumedha230@users.noreply.github.com>
2022-11-24 01:12:21 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,45 @@
+import os
+import discord
+from dotenv.main import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+import random
+
+
+def main():
+    load_dotenv()
+
+    token = "MTA0Mzg1ODk0MDk5MzIyODgwMA.GveYd-.4aYJr0O8BbDz8nJxX1ztO2lX9cpMpzjId8sxeo"
+
+
+    bot = commands.Bot(intents=discord.Intents.all() , command_prefix= "k!" , description='Cute Kirby!')
+
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user.name}has Connected")
+
+    @bot.command()
+    async def ping(ctx):
+        """Checks for a response from the bot"""
+        await ctx.send("Pong")        
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send(f"Hi {ctx.message.author.mention}!")    
+
+    answers = ["patch", "mark", "cat", "dog", "elephant", "frog", "gun"]
+
+    @bot.command(pass_context=True)  
+    async def choose(ctx, k: int):
+        """Chooses from multiple choices."""
+        if 0 <= k <= 10:
+            await ctx.send("This is your random {} pick, {}".format(k, ctx.message.author.mention))
+            embed = discord.Embed(description='\n'.join(random.choices(answers, k=k)))
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Invalid number")    
+
+    bot.run(token)
+if __name__ == '__main__':
+    main()

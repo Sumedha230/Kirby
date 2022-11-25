947813352fb6fcbf0faeb02f4a704c461c029190
Patchh <100405238+Sumedha230@users.noreply.github.com>
2022-11-25 11:11:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os
+import asyncio
 import discord
 from dotenv.main import load_dotenv
 from discord.ext import commands
 from discord.ext.commands import Bot
-
+from discord import app_commands
+import interactions
 import random
 
 
@@ -18,11 +20,12 @@ def main():
     @bot.event
     async def on_ready():
         print(f"{bot.user.name}has Connected")
+    
 
     @bot.command()
     async def ping(ctx):
         """Checks for a response from the bot"""
-        await ctx.send("Pong")        
+        await ctx.send("Pong")     
 
     @bot.command()
     async def hello(ctx):
@@ -44,7 +47,7 @@ def main():
         webhooks = await ctx.channel.webhooks()
         for webhook in webhooks:
             await webhook.delete()
-    
+
     @bot.command()
     async def repeat(ctx, times: int, content='repeating...'):
         if 0<times<=20:
@@ -54,12 +57,12 @@ def main():
             embed = discord.Embed(color = discord.Colour.purple(),description="Kirby is not a fool and won't spam the chat")
             embed.set_image(url ="https://media.discordapp.net/attachments/1042790120652275853/1045611042228666408/kirby-mad.gif")
             await ctx.send(embed=embed)
-    
+
     @bot.command()
     async def avatar(ctx, *,  avamember : discord.Member=None):
-        test = discord.Embed(title=f"{avamember.name}'s Avatar")
-        test.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-        test.set_image(url=ctx.author.avatar.url)
+        test = discord.Embed(title=f"<:kirby_thumbsup:1034765537185632276>  {avamember.name}'s Avatar",color = discord.Colour.purple())
+        test.set_author(name=avamember.name, icon_url=avamember.avatar.url)
+        test.set_image(url=avamember.avatar.url)
         await ctx.send(embed=test)
 
     @bot.command()
@@ -68,13 +71,99 @@ def main():
             user = ctx.author
         user = await bot.fetch_user(user.id)  
         if user.banner:
-            embed = discord.Embed(title = f"{user.name}'s Banner")
-            embed.set_author(name=ctx.author.name,icon_url=ctx.author.avatar.url)
+            embed = discord.Embed(title = f"<:kirby_thumbsup:1034765537185632276>  {user.name}'s Banner",color = discord.Colour.purple())
+            embed.set_author(name=user.name,icon_url=user.avatar.url)
             embed.set_image(url = user.banner.url)
             await ctx.send(embed=embed)
         else:
-            await ctx.send(f'this user has no banner')  
+            await ctx.send(f'<a:idlekirby:1039819001380995122> this user has no banner')
+    
+    @bot.command()
+    async def slap(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1045618240900050954/1045655137953263696/kirby-king-dedede.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045655292773400596/Punishment_Spanked_Sticker_-_Punishment_Spanked_Booty_Slap_-_Discover__Share_GIFs.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045656005356310558/WiffleGif.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045657796986802217/kirboslapping.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} has slapped {user.name}",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)
+
+    @bot.command()
+    async def kiss(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1045618240900050954/1045658804034994226/0d095e578f2c91ad060fada5cde2fd4ebf6f9d18r1-450-375_hq.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045659012743561316/kirby-kiss.gif",
+            "https://cdn.weasyl.com/static/media/bf/d3/6d/bfd36da752a5cfdf862b01ebb4db652308807f907677e1cc0167d20dabc17b94.gif",
+            "https://media.discordapp.net/attachments/1005851300195487875/1045660804852240455/IMG_9527.gif",
+            "https://media.discordapp.net/attachments/1005851300195487875/1045660805162598422/IMG_9528.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} has kissed {user.name}",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)
+
+    @bot.command()
+    async def realkiss(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1005851300195487875/1045660381525331978/IMG_9526.gif",
+            "https://media.discordapp.net/attachments/1005851300195487875/1045660805510742076/IMG_9529.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} has kissed {user.name} hard",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)
+
+    @bot.command()
+    async def fuck(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1045618240900050954/1045661560326078535/how-did-they-how-did.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045661934776754217/8768f928-3eb6-4254-9ffc-a378608a0fa8_text.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045663257639272468/5kozue.jpg"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} tried to e-fuck {user.name} and were caught in 4k",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)   
+
+    @bot.command()
+    async def hug(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1045618240900050954/1045663962882134066/kirby-hug.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045663980926029834/super-smash-bros-kirby.gif",
+            "https://media.discordapp.net/attachments/1045618240900050954/1045666359662616627/a57a2cafc38622f62edecb82be278973.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} hugs {user.name}",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed) 
+
+    @bot.command()
+    async def love(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/1045618240900050954/1045664205388398643/love-kirby.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} sends love to {user.name}",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)
+
+    @bot.command()
+    async def missing(ctx,user:discord.Member=None):
+        randomgifs = [
+            "https://media.discordapp.net/attachments/949680123869814794/1045693438982619146/miss-you-shy-bear-wgvvsi8epdvui25t.gif",
+            "https://media.discordapp.net/attachments/949680123869814794/1045693439276236810/b5395bd842e048cd00cc021b50c37ba6.gif",
+            "https://media.discordapp.net/attachments/949680123869814794/1045693439574024282/missing-you-badly-waiting.gif"
+        ]
+        embed=discord.Embed(title=f"{ctx.author.name} is missing {user.name}",color = discord.Colour.purple())
+        randomgif = random.choice(randomgifs)
+        embed.set_image(url = randomgif)
+        await ctx.send(embed=embed)   
 
     bot.run(token)
+    
+    
 if __name__ == '__main__':
     main()

90d64972c1304bd007392d29523c0701c6aa0ccd
Patchh <100405238+Sumedha230@users.noreply.github.com>
2023-01-04 07:36:59 -0500

    Update help.py

--- help.py
+++ help.py
@@ -14,7 +14,7 @@ class help(commands.Cog,View):
         embed.add_field(name="Moderation Commands",value="clear/purge, warn, ban, kick, emoji_add, mute, unmute, lock, unlock, createrole, addrole, removerole",inline=False)
         embed.add_field(name="User Commands", value = "avatar, guildavatar, banner, serverinfo, userinfo",inline=False)
         embed.add_field(name="Fun Commands",value = "dice, say, repeat, truthordare(tord), wouldyourather(wyr), paranoia, neverhaveiever(nhie), translate, translation, joke, dadjoke, darkjoke, weather", inline=False)
-        embed.add_field(name= "Interaction Command", value = "block, bitch, bite, blush, bonk, boop, bored, cheer, choke, cope, cry, cuddle, dance, deal, eating, eyeroll, fight, facepalm, fake, fart, fuck, highfive, hug, hurt, judge, kill, kiss, laugh, liar, lick, love, marry, missing, nom, pat, pillowfight, pinch, poke, pray, punch, realkiss, salute, sip, sit, shock, slap, smack, smirk, spank, stare, spit, stfu, threaten, tickle, touchgrass, twerk, vibe, wave, wink, yawn",inline=False)
+        embed.add_field(name= "Interaction Command", value = "block, bitch, bite, blush, bonk, boop, bow, bored, cheer, choke, cope, cry, cuddle, dance, deal, eating, eyeroll, fight, facepalm, fake, fart, fuck, highfive, hug, hurt, judge, kill, kiss, laugh, liar, lick, love, marry, missing, nom, pat, pillowfight, pinch, poke, pray, punch, realkiss, salute, sip, sit, shock, slap, smack, smirk, spank, stare, spit, stfu, threaten, tickle, touchgrass, twerk, vibe, wave, wink, yawn",inline=False)
         await ctx.send(embed=embed)  
     @help.command()
     async def purge(self,ctx):
@@ -354,7 +354,13 @@ class help(commands.Cog,View):
     async def bonk(self,ctx):
         embed = discord.Embed(title="Bonk Command",description="This command is an interaction you can do with a user",color = discord.Colour.purple())
         embed.add_field(name="**Syntax**",value="k!bonk @user",inline=False)
-        await ctx.send(embed=embed)    
+        await ctx.send(embed=embed)
+
+    @help.command()
+    async def bow(self,ctx):
+        embed = discord.Embed(title="Bow Command",description="This command is an interaction you can do with a user",color = discord.Colour.purple())
+        embed.add_field(name="**Syntax**",value="k!bow @user",inline=False)
+        await ctx.send(embed=embed)        
 
     @help.command(aliases=['judging'])
     async def judge(self,ctx):

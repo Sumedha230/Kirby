5e672db02bdd9189e88e86be8fb808ddc41cf262
Patchh <100405238+Sumedha230@users.noreply.github.com>
2023-01-15 14:05:55 -0500

    Update animals.py

--- cogs/animals.py
+++ cogs/animals.py
@@ -505,7 +505,39 @@ class animals(commands.Cog):
         embed=discord.Embed(title=f"This is kashi",color = discord.Colour.purple())
         embed.set_image(url = url)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
-        await ctx.send(embed=embed)                                                                              
+        await ctx.send(embed=embed)  
+
+    @commands.command()
+    async def dumdum(self,ctx):
+        KEY = "AIzaSyBWKLC74AeG_xh_QPN37y9aJoIznvQ2KBk"  # click to set to your apikey
+        lmt = 50
+        ckey = "test" 
+        searchTerm = "dumbass"
+           
+        r = requests.get(f"https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" %(searchTerm, KEY, ckey,  lmt))
+        data = r.json()
+        randomgif = random.randint(0,49)
+        url = data['results'][randomgif]["media_formats"]['gif']['url']
+        embed=discord.Embed(title=f"Derron is a dumbass",color = discord.Colour.purple())
+        embed.set_image(url = url)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
+        await ctx.send(embed=embed) 
+
+    @commands.command()
+    async def nessa(self,ctx):
+        KEY = "AIzaSyBWKLC74AeG_xh_QPN37y9aJoIznvQ2KBk"  # click to set to your apikey
+        lmt = 50
+        ckey = "test" 
+        searchTerm = "cute baby"
+           
+        r = requests.get(f"https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" %(searchTerm, KEY, ckey,  lmt))
+        data = r.json()
+        randomgif = random.randint(0,49)
+        url = data['results'][randomgif]["media_formats"]['gif']['url']
+        embed=discord.Embed(title=f"This is NESSA",color = discord.Colour.purple())
+        embed.set_image(url = url)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
+        await ctx.send(embed=embed)                                                                                       
 
 async def setup(bot:commands.Bot) -> None:
     await bot.add_cog(animals(bot))       
